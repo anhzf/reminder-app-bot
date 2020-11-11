c3958900698d0385d5a1c5f45b2aa296fc992f5d
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2020-11-11 09:39:55 +0700

    feat: :sparkles: add ability to log sent message

--- src/Client.js
+++ src/Client.js
@@ -1,6 +1,18 @@
 import { Client } from 'whatsapp-web.js';
 import { sessionData } from './Auth.js';
 
-export default new Client({
-    session: sessionData
+class PoweredClient extends Client {
+    sendMessage() {
+        const [chatId] = arguments;
+        const tt = new Date;
+    
+        console.log(`[${tt.toLocaleString()}]: message sent to ${String(chatId).bgYellow.black}`);
+
+        return super.sendMessage.apply(this, arguments);
+    }
+};
+
+
+export default new PoweredClient({
+   session: sessionData
 });
