5990bb5817041c1ea7022b9bf8dc8e157baa9331
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2020-11-12 16:56:43 +0700

    feat: :sparkles: add align format

--- src/helpers/WhatsappFormat.ts
+++ src/helpers/WhatsappFormat.ts
@@ -1,3 +1,5 @@
+import constants from '../constants.js';
+
 function format(mark: string) {
   return function (text: String): string {
     const trimmed = text.trim();
@@ -5,9 +7,28 @@ function format(mark: string) {
   };
 }
 
+function center(input: string, fillString: string = ' ') {
+  const maxLength = constants.maxRowLength;
+  const isExceedTheMax = input.length >= maxLength;
+
+  return isExceedTheMax ? input : input
+    .padStart((input.length + maxLength) / 2, fillString)
+    .padEnd(maxLength, fillString);
+}
+
+function right(input:string, fillString: string = ' ') {
+  const maxLength = constants.maxRowLength;
+  const isExceedTheMax = input.length >= maxLength;
+
+  return isExceedTheMax ? input : input
+    .padStart(maxLength, fillString);
+}
+
 export default {
   bold: format('*'),
   italic: format('_'),
   strike: format('~'),
   code: format('```'),
+  center,
+  right,
 } as const;
